dbefab20725a37f4a8253997cd21198140627cee
Konstantinos Efkarpidis <kefkarpidis@deloitte.gr>
2022-03-19 14:48:55 +0200

    React router, redirect

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
 const Products = () => {
-  return <h1>The products Page</h1>;
+  return (
+    <section>
+      <h1>The products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/3">Pizza</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
